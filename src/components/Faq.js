dcff051b4adb5cc34184d193cc83a1682677178a
Moses Surumen <moses.susrumen@gmail.com>
2019-04-24 16:01:50 -0700

    final look

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -38,22 +38,22 @@ class Faq extends Component {
                         <li id="1-1">
                           <div className="faq-panes-list-item-header">
                             <span className="faq-panes-list-item-counter" />
-                            <h3>How to sign up on mssg.me platform?</h3>
+                            <h3>Create a Reward Plan</h3>
                             <span className="faq-panes-list-item-status" />
                           </div>
                           <div className="faq-panes-list-item-content">
                             <div><span className="ace-all-bold-hthree"><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr"><b>Sign up </b></span><span className=" author-d-4z65zz66zl57z75zyiz66zfr2fz87zwz89znuiz88zlz79zz75zz89zhqpz75ztjz79zgdz122zz84zz89zsz86zz72zpz80zfz87zcz85z0z74zu"><b>with</b></span><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr"><b> Facebook</b></span></span></div>
                             <ol className="listtype-number listindent1 list-number1" start={1}>
-                              <li><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">Click the button <a href="https://app.mssg.me/registration?lang=en"><strong>Get started</strong></a>&nbsp;</span><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">on mssg.me website</span></li>
+                              <li><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">Click the button <a href="/login"><strong>Get started</strong></a>&nbsp;</span><span className="author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">on our website</span></li>
                               <li><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">Press the button </span><span className="highlight author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr"><b>Facebook</b></span></li>
                               <li><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">Choose your Plan</span></li>
                             </ol>
                             <div><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">If you don’t have email verification on Facebook, </span><span className=" author-d-4z65zz66zl57z75zyiz66zfr2fz87zwz89znuiz88zlz79zz75zz89zhqpz75ztjz79zgdz122zz84zz89zsz86zz72zpz80zfz87zcz85z0z74zu">please, </span><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">write your email</span><span className=" author-d-4z65zz66zl57z75zyiz66zfr2fz87zwz89znuiz88zlz79zz75zz89zhqpz75ztjz79zgdz122zz84zz89zsz86zz72zpz80zfz87zcz85z0z74zu">.</span></div>
-                            <div><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">Also</span> <span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">you</span><span className=" author-d-4z65zz66zl57z75zyiz66zfr2fz87zwz89znuiz88zlz79zz75zz89zhqpz75ztjz79zgdz122zz84zz89zsz86zz72zpz80zfz87zcz85z0z74zu"> don’t </span><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">need to create and remember your password from mssg.me profile.</span></div>
+                            <div><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">Also</span> <span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">you</span><span className=" author-d-4z65zz66zl57z75zyiz66zfr2fz87zwz89znuiz88zlz79zz75zz89zhqpz75ztjz79zgdz122zz84zz89zsz86zz72zpz80zfz87zcz85z0z74zu"> don’t </span><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">need to create and remember your password from Grade2Play profile.</span></div>
                             <div />
                             <div><span className="ace-all-bold-hthree"><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr"><b>Sing up </b></span><span className=" author-d-4z65zz66zl57z75zyiz66zfr2fz87zwz89znuiz88zlz79zz75zz89zhqpz75ztjz79zgdz122zz84zz89zsz86zz72zpz80zfz87zcz85z0z74zu"><b>with</b></span><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr"><b> email</b></span></span></div>
                             <ol className="listtype-number listindent1 list-number1" start={1}>
-                              <li><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">Click the button <a href="https://app.mssg.me/registration?lang=en"><strong>Get started</strong></a>&nbsp;</span><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">on mssg.me website</span></li>
+                              <li><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">Click the button <a href="/login"><strong>Get started</strong></a>&nbsp;</span><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">on our website</span></li>
                               <li><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">Fill in all the fields</span> <span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr h-lparen">(email,</span><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">&nbsp;name, page name, password)</span></li>
                               <li><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr">Read and agree with our </span><span className="highlight author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr"><b>Terms of Use</b></span><span className=" author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr"> and</span><span className="highlight author-d-1gg9uz65z1iz85zgdz68zmqkz84zo2qovvldyhz76zz87zz82zug4rnz77zqpz85zz68zkz86zz89zz81z6wz69zvz67zwnkr"><b> Privacy Policy</b></span></li>
                               <li><span className=" author-d-4z65zz66zl57z75zyiz66zfr2fz87zwz89znuiz88zlz79zz75zz89zhqpz75ztjz79zgdz122zz84zz89zsz86zz72zpz80zfz87zcz85z0z74zu">Click on the button </span><span className="highlight author-d-4z65zz66zl57z75zyiz66zfr2fz87zwz89znuiz88zlz79zz75zz89zhqpz75ztjz79zgdz122zz84zz89zsz86zz72zpz80zfz87zcz85z0z74zu"><b>Continue</b></span></li>
@@ -64,12 +64,12 @@ class Faq extends Component {
                         <li id="1-2">
                           <div className="faq-panes-list-item-header">
                             <span className="faq-panes-list-item-counter" />
-                            <h3>How to change an email?</h3>
+                            <h3>Select in-school goals for your child</h3>
                             <span className="faq-panes-list-item-status" />
                           </div>
                           <div className="faq-panes-list-item-content">
                             <ol className="ul1">
-                              <li><a href="https://app.mssg.me/login"><strong>Log in</strong></a> your personal profile</li>
+                              <li><a href="/login"><strong>Log in</strong></a> your personal profile</li>
                               <li className="li1">Click on the button <b>Settings</b></li>
                               <li className="li1">Choose a section <b>Account</b></li>
                               <li className="li1">Set up your new <b>email</b></li>
@@ -84,7 +84,7 @@ class Faq extends Component {
                         <li id="3-1">
                           <div className="faq-panes-list-item-header">
                             <span className="faq-panes-list-item-counter" />
-                            <h3>How to set up widget?</h3>
+                            <h3>FREE plan</h3>
                             <span className="faq-panes-list-item-status" />
                           </div>
                           <div className="faq-panes-list-item-content" style={{display: 'none'}}>
@@ -101,7 +101,7 @@ class Faq extends Component {
                         <li id="3-2">
                           <div className="faq-panes-list-item-header">
                             <span className="faq-panes-list-item-counter" />
-                            <h3>How to install widget on your website?</h3>
+                            <h3>&#x00024; 40 per month</h3>
                             <span className="faq-panes-list-item-status" />
                           </div>
                           <div className="faq-panes-list-item-content" style={{display: 'none'}}>
@@ -114,21 +114,11 @@ class Faq extends Component {
                         <li id="3-3">
                           <div className="faq-panes-list-item-header">
                             <span className="faq-panes-list-item-counter" />
-                            <h3>Can I try the widget for free?</h3>
+                            <h3>&#x00024; 300 per academic year</h3>
                             <span className="faq-panes-list-item-status" />
                           </div>
                           <div className="faq-panes-list-item-content" style={{display: 'none'}}>
                             <p>Yes! After your first registration you’ll have an opportunity to try <strong>Widget</strong> in <strong>14-Day trial of PRO </strong>tariff</p>
-                          </div>
-                        </li>
-                        <li id="3-4">
-                          <div className="faq-panes-list-item-header">
-                            <span className="faq-panes-list-item-counter" />
-                            <h3>How much is widget costs?</h3>
-                            <span className="faq-panes-list-item-status" />
-                          </div>
-                          <div className="faq-panes-list-item-content" style={{display: 'none'}}>
-                            <p>You can use widget fo FREE.</p>
                           </div>
                         </li>
                       </ul>
